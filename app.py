a35bfd9c47e27d80088325891c2c9a7954f82179
nonwor <nonwor@nonwors-MBP.attlocal.net>
2024-01-05 10:54:12 -0500

    GET and POST request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, select
@@ -24,22 +24,39 @@ class users(db.Model):
 with app.app_context():
     db.create_all()
 
-@app.route("/")
+#Get users as json
+@app.route("/users", methods=["GET"])
 def hello_world():
 	print('Hello world!', file=sys.stderr)
-	stmt = select(users)
-	all_users = db.session.execute(db.select(users).order_by(users.username)).scalars()
-	for user in all_users:
-		print(user.email, file=sys.stderr)
-	return ("ok")
+	#We can do a get all user request here
+	#We don't really need to check for GET, since it is handled by Flask
+	if request.method == "GET":
+		all_users = db.session.execute(db.select(users).order_by(users.username)).scalars()
+		json = {}
+		for user in all_users:
+			json[user.id] = {"username":user.username, "email":user.email}
+			print(user.email, file=sys.stderr)
+		print(json, file=sys.stderr)
+		return (json,200)
 
-@app.route("/user/<username>")
-def showRouteInput(username):
+#Get users as a CSV
+
+#Create new user, or if a user exists we return some message and data
+@app.route("/user", methods=["POST"])
+def showRouteInput():
 	# Handle API input process data according to specs
-	if username == " ":
-		return "Bad request" , 400
-	else:
-		print("input:"+username, file=sys.stderr)
-		
-		return username , 200
+	data = request.get_json()
+	print(data, file=sys.stderr)
+	user = users(
+		username=data["name"],
+		email=data["email"],
+	)
+	try:
+		db.session.add(user)
+		db.session.commit()
+		return("success", 200)
+	except:
+		return("Failed to add user", 400)
+	
+	return("ok",200)
 
